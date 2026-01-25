69fc43fc9a5bd8eea2f44da61d736499520fdc01
Marc Hermans <marc.hermans@ldtteam.com>
2026-01-25 15:18:44 +0100

    Properly close self-closing paragraphs when rendering them out to Markdown.

--- doclet/src/main/java/com/communi/suggestu/javamark/doclet/writers/MarkdownSubWriterHolderWriter.java
+++ doclet/src/main/java/com/communi/suggestu/javamark/doclet/writers/MarkdownSubWriterHolderWriter.java
@@ -1,16 +1,24 @@
 package com.communi.suggestu.javamark.doclet.writers;
 
 import com.sun.source.doctree.DocTree;
+import com.sun.source.doctree.DocTreeVisitor;
+import com.sun.source.doctree.EndElementTree;
+import com.sun.source.doctree.StartElementTree;
 import jdk.javadoc.internal.doclets.formats.html.HtmlConfiguration;
 import jdk.javadoc.internal.doclets.formats.html.SubWriterHolderWriter;
 import jdk.javadoc.internal.doclets.formats.html.markup.Entity;
 import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle;
 import jdk.javadoc.internal.doclets.formats.html.markup.HtmlTree;
+import jdk.javadoc.internal.doclets.formats.html.markup.RawHtml;
 import jdk.javadoc.internal.doclets.toolkit.Content;
 import jdk.javadoc.internal.doclets.toolkit.util.DocPath;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Name;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
 
 public class MarkdownSubWriterHolderWriter extends SubWriterHolderWriter
 {
@@ -37,6 +45,8 @@ public class MarkdownSubWriterHolderWriter extends SubWriterHolderWriter
      */
     private void addCommentTags(Element element, List<? extends DocTree> tags, boolean depr,
         boolean first, boolean inSummary, Content target) {
+        tags = closeDanglingParagraphs(tags);
+
         if (options.noComment()) {
             return;
         }
@@ -48,4 +58,62 @@ public class MarkdownSubWriterHolderWriter extends SubWriterHolderWriter
             target.add(Entity.NO_BREAK_SPACE);
         }
     }
+
+    private List<DocTree> closeDanglingParagraphs(List<? extends DocTree> tags) {
+        var result = new ArrayList<DocTree>();
+        var tagStack = new Stack<Name>();
+        for (final DocTree tag : tags)
+        {
+            if (tag instanceof StartElementTree startElementTree) {
+                if (!tagStack.isEmpty())
+                {
+                    var lastTag = tagStack.peek();
+                    if (Objects.equals(lastTag.toString(), "p"))
+                    {
+                        //Last tag was a paragraph that was not closed.
+                        result.add(new EndDocElement(lastTag));
+                        tagStack.pop();
+                    }
+                }
+
+                tagStack.add(startElementTree.getName());
+            } else if (tag instanceof EndElementTree endElementTree) {
+                if (tagStack.isEmpty())
+                    throw new IllegalStateException("Closing tag without opening!");
+
+                var previousOpen = tagStack.pop();
+                if (!previousOpen.equals(endElementTree.getName()))
+                    throw new IllegalStateException("Tag: " + previousOpen + " but the next closing tag was: " + endElementTree.getName());
+            }
+
+            result.add(tag);
+        }
+
+        while(!tagStack.isEmpty()) {
+            result.add(new EndDocElement(tagStack.pop()));
+        }
+
+        return result;
+    }
+
+    private record EndDocElement(Name name) implements EndElementTree
+    {
+        @Override
+            public Name getName()
+            {
+                return name;
+            }
+
+            @Override
+            public Kind getKind()
+            {
+                return Kind.END_ELEMENT;
+            }
+
+            @Override
+            public <R, D> R accept(final DocTreeVisitor<R, D> visitor, final D data)
+            {
+                return visitor.visitEndElement(this, data);
+            }
+        }
 }
--- example/src/main/java/com/communi/suggestu/javamark/example/IComplexMethodDocumentationInterface.java
+++ example/src/main/java/com/communi/suggestu/javamark/example/IComplexMethodDocumentationInterface.java
@@ -12,10 +12,10 @@ public interface IComplexMethodDocumentationInterface
      * Returns the current {@link Object} if there is one.
      * If a new chiseling operation is started no {@link Object} is available,
      * as such an empty {@link Optional} will be returned in that case.
-     *
+     * <p>
      * Only after the primary call to something or something
      * the returned {@link Optional} can contain a {@link Object}.
-     *
+     * <p>
      * @return The {@link Optional} containing the {@link Object}.
      */
     Optional<Object> getMutator();
